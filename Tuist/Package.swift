d7d3b5ee20fa446e09bc439c9d46b4070d5324a6
Daniel Bachar <danielbac@zillowgroup.com>
2024-01-25 15:53:46 -0700

    slim package.swift

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -4,15 +4,6 @@ import PackageDescription
 let package = Package(
     name: "PackageName",
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.8.0"),
-        .package(url: "https://github.com/facebook/facebook-ios-sdk", from: "16.1.3"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.15.0"),
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", .upToNextMinor(from: "1.5.0")),
-        .package(url: "https://github.com/iterable/swift-sdk", from: "6.4.15"),
-        .package(url: "https://github.com/Trendyol/ios-components", revision: "c9260bfe203a16a278eca5542c98455eece98aa4"),
-        .package(url: "https://github.com/stripe/stripe-ios", from: "23.12.0"),
-        .package(url: "https://github.com/groue/GRDB.swift", from: "6.16.0"),
-        .package(url: "https://github.com/auth0/Auth0.swift", from: "2.5.0"),
         .package(path: "../../../Modules/LocalSwiftPackageFlag"),
         .package(path: "../../../Modules/LocalSwiftPackageBootstrap"),
     ]
